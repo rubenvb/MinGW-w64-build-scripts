2999209fd1d9ca2a12dc862544a629972d9254c4
Ruben Van Boxem <vanboxem.ruben@gmail.com>
2011-07-24 23:06:06 +0200

    small fixes

--- toolchain/scripts/common.sh
+++ toolchain/scripts/common.sh
@@ -15,7 +15,7 @@ export SRC_DIR=$TOP_DIR/src
 export BUILD_DIR=$TOP_DIR/$SHORT_NAME
 export LOG_DIR=$BUILD_DIR/logs
 export GCC_LIBS=$BUILD_DIR/libs
-export GRAPHITE_LIBS="--with-ppl=$GCC_LIBS --with-cloog=$GCC_LIBS --enable-cloog-backend=isl --with-host-libstdcxx=-lstdc++"
+export GRAPHITE_LIBS="--with-ppl=$GCC_LIBS --with-cloog=$GCC_LIBS --enable-cloog-backend=isl --with-host-libstdcxx="'-lstdc++ -lsupc++'
 export SCRIPTS=$TOP_DIR/scripts
 
     export GCC_SRC=$SRC_DIR/gcc
@@ -30,9 +30,9 @@ mkdir -p $DIRS_TO_MAKE
 
 # optimized for my system.
 export BUILD_CFLAGS='-O2 -mtune=core2 -fomit-frame-pointer -momit-leaf-frame-pointer -fgraphite-identity -floop-interchange -floop-block -floop-parallelize-all '
-export BUILD_LFLAGS=
+export BUILD_LDFLAGS=
 export BUILD_CFLAGS_LTO=$BUILD_CFLAGS'-flto'
-export BUILD_LFLAGS_LTO=$BUILD_LFLAGS'-flto='$BUILD_CORES
+export BUILD_LDFLAGS_LTO=$BUILD_LDFLAGS'-flto='$BUILD_CORES
 export MAKE_OPTS="-j"$BUILD_CORES
 
 # get version info
