4c19c7b04fa0ecf24bb5358b214a80c178feca74
Ruben Van Boxem <vanboxem.ruben@gmail.com>
2011-10-27 15:27:36 +0200

    fix crt build for linux32mingw32

--- toolchain/buildall.sh
+++ toolchain/buildall.sh
@@ -5,12 +5,14 @@ echo "Building linux 64-bit to Windows 32-bit cross-compiler"
 sh ./buildlinux64mingw32.sh || exit 1
 echo "Building linux 64-bit to Windows 64-bit cross-compiler"
 sh ./buildlinux64mingw64.sh || exit 1
+
+export PATH=$PWD/linux64mingw32/mingw32/bin:$PWD/linux64mingw64/mingw64/bin:$PATH
+# apparently, below things are "cross-compiled" anyway... so I'll need the windows cross-compilers in PATH
 echo "Building linux 32-bit to Windows 32-bit cross-compiler"
 sh ./buildlinux32mingw32.sh || exit 1
 echo "Building linux 32-bit to Windows 64-bit cross-compiler"
 sh ./buildlinux32mingw64.sh || exit 1
 
-export PATH=$PWD/linux64mingw32/mingw32/bin:$PWD/linux64mingw64/mingw64/bin:$PATH
 
 echo "Building Windows 64-bit to Windows 64-bit native compiler"
 sh ./buildmingw64mingw64.sh || exit 1
--- toolchain/buildlinux32mingw32.sh
+++ toolchain/buildlinux32mingw32.sh
@@ -9,7 +9,7 @@ export HOST=$HOST_ARCH-$HOST_VENDOR-$HOST_OS
 export TARGET=i686-w64-mingw32
 export SHORT_NAME=mingw32
 export LONG_NAME=linux32mingw32
-export CRT_MULTILIB='--enable-lib64 --disable-lib32'
+export CRT_MULTILIB='--disable-lib64 --enable-lib32'
 export GDB_PYTHON_WIN64_WORKAROUND=
 
 # call main build script
