b5850b161e6c934996fe0a4929fbcc836cc758f9
Ruben Van Boxem <vanboxem.ruben@gmail.com>
2011-10-28 21:42:12 +0200

    Disable graphite optimizations for Mac builds, the darwinx GCC does not support graphite :(, it's GCC 4.2

--- toolchain/scripts/common.sh
+++ toolchain/scripts/common.sh
@@ -4,8 +4,12 @@ set -e
 # build options - for my system only
 export BUILD=x86_64-linux-gnu
 export MAKE_OPTS="-j2"
-export HOST_CFLAGS="-O2 -mtune=core2 -fomit-frame-pointer -momit-leaf-frame-pointer -fgraphite-identity -floop-interchange -floop-block -floop-parallelize-all" #-flto
+export HOST_CFLAGS="-O2 -mtune=core2" #-flto
 export HOST_LDFLAGS= #"-flto"
+if [ "$HOST_VENDOR" != "apple" ]
+then
+    export HOST_CFLAGS="$HOST_CFLAGS -fomit-frame-pointer -momit-leaf-frame-pointer -fgraphite-identity -floop-interchange -floop-block -floop-parallelize-all"
+fi
 # GCC languages to be built
 export GCC_LANGUAGES='c,lto,c++,fortran,objc,obj-c++,java'
 
