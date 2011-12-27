070ef901d4687e2ac483cfeb70952508f1109a53
Ruben Van Boxem <vanboxem.ruben@gmail.com>
2011-12-27 11:21:21 +0100

    revert cloog version bump (GCC fails to find it) and move lto to cygwin and linux hosts only

--- toolchain/scripts/common.sh
+++ toolchain/scripts/common.sh
@@ -4,14 +4,18 @@ set -e
 # build options - for my system only
 export BUILD=x86_64-linux-gnu
 export MAKE_OPTS="-j2"
-export HOST_CFLAGS="-O2 -mtune=core2 -flto"
+export HOST_CFLAGS="-O2 -mtune=core2"
 export HOST_LDFLAGS= #"-flto"
 if [ "$HOST_VENDOR" != "apple" ]
 then
-    export HOST_CFLAGS="$HOST_CFLAGS -fomit-frame-pointer -momit-leaf-frame-pointer -fgraphite-identity -floop-interchange -floop-block -floop-parallelize-all"
+  export HOST_CFLAGS="$HOST_CFLAGS -fomit-frame-pointer -momit-leaf-frame-pointer -fgraphite-identity -floop-interchange -floop-block -floop-parallelize-all"
+  if [ "$HOST_OS" != "mingw32" ]
+  then
+    export HOST_CFLAGS="$HOST_CFLAGS -flto"
+  fi
 fi
 # GCC languages to be built
-export GCC_LANGUAGES='c,lto,c++,fortran,objc,obj-c++,java'
+export GCC_LANGUAGES='c,lto,c++,objc,obj-c++,java' #fortran
 
 # get version info
 echo "-> Loading version info"
@@ -19,9 +23,6 @@ echo "-> Loading version info"
 # set and create directories
 echo "-> Setting up directories"
 . ./scripts/directories.sh || exit 1
-# symlinks
-#echo "-> Setting up source tree symlinks"
-#. ./scripts/symlinks.sh || exit 1
 # multilib wrapper scripts
 # echo "-> Setting up multilib wrapper scripts"
 # . ./scripts/fakegcc.sh
