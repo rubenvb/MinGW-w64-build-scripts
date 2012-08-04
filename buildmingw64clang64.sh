b0f6c10be7b8b59a0eccfa55328e5bad4c58d156
Ruben Van Boxem <vanboxem.ruben@gmail.com>
2012-08-04 19:32:49 +0200

    add 64-bit Clang and enable in buildall.sh

--- buildmingw64clang64.sh
+++ buildmingw64clang64.sh
@@ -2,19 +2,18 @@
 set -e
 
 # platform information
-export HOST_ARCH=i686
+export HOST_ARCH=x86_64
 export HOST_VENDOR=w64
 export HOST_OS=mingw32
 export HOST=$HOST_ARCH-$HOST_VENDOR-$HOST_OS
 
-export TARGET_ARCH=i686
+export TARGET_ARCH=x86_64
 export TARGET_VENDOR=w64
 export TARGET_OS=mingw32
-
 export TARGET=$TARGET_ARCH-$TARGET_VENDOR-$TARGET_OS
 
-export SHORT_NAME=mingw32-dw2
-export LONG_NAME=mingw32clang32
+export SHORT_NAME=mingw64
+export LONG_NAME=mingw64clang64
 
 # call main build script
 . ./scripts/buildclangfromcross.sh
