bb441185f026d8f118c7a58207ab9239ba42beb4
Ruben Van Boxem <vanboxem.ruben@gmail.com>
2012-08-04 23:08:06 +0200

    remove dw2 cross compiler from buildall.sh

--- buildall.sh
+++ buildall.sh
@@ -38,12 +38,15 @@ sh ./buildmingw64mingw32.sh || exit 1
 #echo "Building Mac 64-bit to Windows 64-bit cross-compiler"
 #sh ./buildmac64mingw64.sh || exit 1
 
-echo "Building Windows 32-bit to Windows 32-bit dw2 native compiler"
-sh ./buildmingw32mingw32-dw2.sh || exit 1
 #echo "Building linux 64-bit to Windows 32-bit dw2 cross-compiler"
 #sh ./buildlinux64mingw32-dw2.sh
 #echo "Building linux 32-bit to Windows 32-bit dw2 cross-compiler"
 #sh ./buildlinux32mingw32-dw2.sh
+echo "Building Windows 32-bit to Windows 32-bit dw2 native compiler"
+sh ./buildmingw32mingw32-dw2.sh || exit 1
+#echo "Building Windows 64-bit to Windows 32-bit dw2 native compiler"
+#sh ./buildmingw64mingw32-dw2.sh || exit 1
+
 
 echo "Building Windows 32-bit to Windows 32-bit native LLVM/Clang"
 sh ./buildmingw32clang32.sh
