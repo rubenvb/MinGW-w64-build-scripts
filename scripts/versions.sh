f9b773345b55969fad570a07db409f14b5946165
Ruben Van Boxem <vanboxem.ruben@gmail.com>
2012-08-04 14:20:46 +0200

    align versions

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -3,7 +3,7 @@ set -e
 
 # versions of different packages (prefix RUBENVB to prevent conflict with macros)
 export RUBENVB_GCC_VERSION="4.6.3"
-export RUBENVB_CLANG_VERSION="3.2pre"
+export RUBENVB_CLANG_VERSION="3.1"
 
 # my revision number
 export MY_REVISION=
