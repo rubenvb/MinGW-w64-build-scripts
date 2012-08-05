9758b4f54a6b286073101516797c3b6796d264bf
Ruben Van Boxem <vanboxem.ruben@gmail.com>
2012-08-05 23:28:20 +0200

    set release version

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -2,5 +2,5 @@
 set -e
 
 # versions of different packages (prefix RUBENVB to prevent conflict with macros)
-export RUBENVB_GCC_VERSION="4.6.3"
-export RUBENVB_CLANG_VERSION="3.1"
+export RUBENVB_GCC_VERSION="4.6.3-1-release"
+export RUBENVB_CLANG_VERSION="3.1-release"
