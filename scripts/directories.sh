25f906d659c4dd563ae9fdd909120690aaefc016
Ruben Van Boxem <vanboxem.ruben@gmail.com>
2012-08-05 23:56:39 +0200

    fix missing mingw/include dir

--- scripts/directories.sh
+++ scripts/directories.sh
@@ -23,7 +23,7 @@ export SCRIPTS=$TOP_DIR/scripts
 export PREFIX=$BUILD_DIR/$SHORT_NAME
 
 DIRS_TO_MAKE="$BUILD_DIR $LOG_DIR
-              $PREFIX $PREFIX/mingw $PREFIX/$TARGET/include $PREFIX/$TARGET/lib
+              $PREFIX $PREFIX/mingw $PREFIX/mingw/include $PREFIX/$TARGET/include $PREFIX/$TARGET/lib
               $PREREQ_INSTALL $PREREQ_INSTALL/lib $PREREQ_INSTALL/include
               $PACKAGE_DIR $PACKAGE_DIR/$HOST"
 mkdir -p $DIRS_TO_MAKE
