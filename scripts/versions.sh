691026bc194b9d2a398b26725b01a2aa513f7304
Ruben Van Boxem <vanboxem.ruben@gmail.com>
2012-08-05 21:12:18 +0200

    remove revision cause it can fit in the version string

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -4,6 +4,3 @@ set -e
 # versions of different packages (prefix RUBENVB to prevent conflict with macros)
 export RUBENVB_GCC_VERSION="4.6.3"
 export RUBENVB_CLANG_VERSION="3.1"
-
-# my revision number
-export MY_REVISION=
