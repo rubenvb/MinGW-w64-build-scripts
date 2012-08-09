987a4a4eb5feedbe323d7ee16942bd63f8568760
Ruben Van Boxem <vanboxem.ruben@gmail.com>
2012-08-09 20:31:08 +0200

    set version and toolchains to build

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -2,7 +2,8 @@
 set -e
 
 # versions of different packages (prefix RUBENVB to prevent conflict with macros)
-export RUBENVB_GCC_VERSION="4.6.3"
-export RUBENVB_CLANG_VERSION="3.1"
+export RUBENVB_GCC_VERSION="4.7.1-1-release"
+export RUBENVB_CLANG_VERSION=""
 
 export RUBENVB_PACKAGE_OPTIONS="--with-pkgversion=rubenvb-$RUBENVB_GCC_VERSION --with-bugurl=mingw-w64-public@lists.sourceforge.net"
+
