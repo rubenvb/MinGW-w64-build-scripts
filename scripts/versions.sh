d43ff2bb2e91812a426df0b95f81238321c99bb5
Ruben Van Boxem <vanboxem.ruben@gmail.com>
2012-08-17 17:15:21 +0200

    set 4.7-1-stdthread release version

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -2,7 +2,7 @@
 set -e
 
 # versions of different packages (prefix RUBENVB to prevent conflict with macros)
-export RUBENVB_GCC_VERSION="4.7-stdthread"
+export RUBENVB_GCC_VERSION="4.7-1-stdthread"
 export RUBENVB_CLANG_VERSION=""
 
 export RUBENVB_PACKAGE_OPTIONS="--with-pkgversion=rubenvb-$RUBENVB_GCC_VERSION --with-bugurl=mingw-w64-public@lists.sourceforge.net"
