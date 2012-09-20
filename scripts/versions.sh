19aedb39ee5a0a579e8deeff848c7de80e43fc45
Ruben Van Boxem <vanboxem.ruben@gmail.com>
2012-09-20 15:38:11 +0200

    small fixes

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -2,7 +2,7 @@
 set -e
 
 # versions of different packages (prefix RUBENVB to prevent conflict with macros)
-export RUBENVB_GCC_VERSION="4.6.3"
-export RUBENVB_CLANG_VERSION="3.1"
+export RUBENVB_GCC_VERSION="4.7"
+export RUBENVB_CLANG_VERSION="3.2pre"
 
 export RUBENVB_PACKAGE_OPTIONS="--with-pkgversion=rubenvb-$RUBENVB_GCC_VERSION --with-bugurl=mingw-w64-public@lists.sourceforge.net"
