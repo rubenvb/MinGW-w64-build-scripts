13b1b423a8e098e4853c1893dac42116a04c4740
Ruben Van Boxem <vanboxem.ruben@gmail.com>
2012-09-20 15:44:55 +0200

    set toolchains to build

--- buildall.sh
+++ buildall.sh
@@ -38,19 +38,19 @@ sh ./buildcygwin32mingw64.sh || exit 1
 #echo "Building Mac 64-bit to Windows 64-bit cross-compiler"
 #sh ./buildmac64mingw64.sh || exit 1
 
-#echo "Building linux 64-bit to Windows 32-bit dw2 cross-compiler"
-#sh ./buildlinux64mingw32-dw2.sh
+echo "Building linux 64-bit to Windows 32-bit dw2 cross-compiler"
+sh ./buildlinux64mingw32-dw2.sh
 #echo "Building linux 32-bit to Windows 32-bit dw2 cross-compiler"
 #sh ./buildlinux32mingw32-dw2.sh
 
 export PATH=$PWD/linux64mingw32-dw2/mingw32-dw2/bin:$PATH
 
-#echo "Building Windows 32-bit to Windows 32-bit dw2 native compiler"
-#sh ./buildmingw32mingw32-dw2.sh || exit 1
+echo "Building Windows 32-bit to Windows 32-bit dw2 native compiler"
+sh ./buildmingw32mingw32-dw2.sh || exit 1
 #echo "Building Windows 64-bit to Windows 32-bit dw2 native compiler"
 #sh ./buildmingw64mingw32-dw2.sh || exit 1
 
-#echo "Building Windows 32-bit to Windows 32-bit native LLVM/Clang"
-#sh ./buildmingw32clang32.sh
-#echo "Building Windows 64-bit to Windows 64-bit native LLVM/Clang"
-#sh ./buildmingw64clang64.sh
+echo "Building Windows 32-bit to Windows 32-bit native LLVM/Clang"
+sh ./buildmingw32clang32.sh
+echo "Building Windows 64-bit to Windows 64-bit native LLVM/Clang"
+sh ./buildmingw64clang64.sh
