0d653d454d0a294a8908f75726a636cd71a655f1
Ruben Van Boxem <vanboxem.ruben@gmail.com>
2013-05-01 11:50:10 +0200

    fix licenses and set versions for next GCC 4.8 update (unreleased)

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -2,7 +2,7 @@
 set -e
 
 # versions of different packages (prefix RUBENVB to prevent conflict with macros)
-export RUBENVB_GCC_VERSION="4.8.0"
+export RUBENVB_GCC_VERSION="4.8-unstable"
 export RUBENVB_CLANG_VERSION="3.3pre"
 
 export RUBENVB_PACKAGE_OPTIONS="--with-pkgversion=rubenvb-$RUBENVB_GCC_VERSION --with-bugurl=mingw-w64-public@lists.sourceforge.net"
