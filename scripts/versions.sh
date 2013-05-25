8e458fab4b18777c7c1a62dc68cb4e9d8f953ee3
Ruben Van Boxem <vanboxem.ruben@gmail.com>
2013-05-25 16:54:59 +0200

    set GCC 4.6.4 release versions

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -2,7 +2,7 @@
 set -e
 
 # versions of different packages (prefix RUBENVB to prevent conflict with macros)
-export RUBENVB_GCC_VERSION="4.6.3-2-release"
-export RUBENVB_CLANG_VERSION="3.2-release"
+export RUBENVB_GCC_VERSION="4.6.4-release"
+export RUBENVB_CLANG_VERSION="3.2-1-release"
 
 export RUBENVB_PACKAGE_OPTIONS="--with-pkgversion=rubenvb-$RUBENVB_GCC_VERSION --with-bugurl=mingw-w64-public@lists.sourceforge.net"
